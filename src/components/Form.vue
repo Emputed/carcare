4e2803bd1288eb4d94903fc202e9cef663a6e8d3
emputed <amejoradan1600@alumno.ipn.mx>
2023-06-11 00:28:04 -0600

    Ya no muestra los nombres en los Selct

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,14 @@
 <template>
-    <select  class="form-select form-select-sm" aria-label=".form-select-sm example">
-        <option selected >Open this select menu</option>
-        <option v-for="auto in autos" value="{{ auto.text }}">{{ auto.text }}</option>
+    <span>{{ marca }}</span>
+
+    <select v-model="marca" class="form-select form-select-sm" aria-label=".form-select-sm example">
+        <option selected >Elige la marca</option>
+        <option v-for="auto in autos" >{{ auto.text }}</option>
+    </select>
+
+    <select v-model="ano" class="form-select form-select-sm" aria-label=".form-select-sm example">
+        <option selected >Elige el Year</option>
+        <option v-for="year in years" >{{ year.text }}</option>
     </select>
 </template>
     
@@ -12,19 +19,24 @@ export default {
     name: 'Form',
     data() {
         return {
-            autos: []
+            autos: [],
+            marca: '',
+            years: []
         }
     },
     methods: {
         async getCarro() {
-            const res = await axios.get('http://core-axa-dev.eba-vu2huqdp.us-east-1.elasticbeanstalk.com/v1/axa-car/brands')
-            this.autos = res.data
+            const carro = await axios.get('http://core-axa-dev.eba-vu2huqdp.us-east-1.elasticbeanstalk.com/v1/axa-car/brands')
+            this.autos = carro.data
             console.log(this.autos)
+        },
+        async getYear(){
+            const res = await axios.get(`http://core-axa-dev.eba-vu2huqdp.us-east-1.elasticbeanstalk.com/v1/axa-car/years?brand=${this.marca}`)
+            this.year = res.data
+            console.log(this.year)
         }
     },
-    mounted() {
-        this.getCarro()
-    }
+
 }
 
 </script>
