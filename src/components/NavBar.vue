d0d1995032cc893013063cfc0de733d9fd928e6c
Daniel Flores Rodríguez <daniflores374@gmail.com>
2023-06-18 18:16:45 -0600

    HomeView innit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar bg-body-tertiary vh-10">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="@/assets/logo.svg" alt="Logo" width="30" height="24"
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,82 @@
 <template>
   <div class="home">
-    <NavBar></NavBar>
-    <img alt="Vue logo" src="../assets/logo.svg">
 
-    <div class="container text-center">
-      <div class="row align-items-center">
-        <div class="col">
-          
+    <NavBar></NavBar>
+
+    <!-- home 1 -->
+    <div class="container-fluid text-center min-vh-80 w-100">
+
+      <!-- Titulo -->
+      <div class="container text-center mw-80 mh-10">
+        <h2 class="mt-4">
+          ¿QUIÉNES SOMOS?
+        </h2>
+      </div>
+
+      <div class="container-fluid text-center rounded bg-light h-40 mw-4 p-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quae repudiandae maiores minima consectetur
+        illo quod dolore possimus quidem, esse quasi nemo ab nam eveniet blanditiis molestias aperiam, repellat culpa.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quae repudiandae maiores minima consectetur
+        illo quod dolore possimus quidem, esse quasi nemo ab nam eveniet blanditiis molestias aperiam, repellat culpa.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quae repudiandae maiores minima consectetur
+        illo quod dolore possimus quidem, esse quasi nemo ab nam eveniet blanditiis molestias aperiam, repellat culpa.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quae repudiandae maiores minima consectetur
+        illo quod dolore possimus quidem, esse quasi nemo ab nam eveniet blanditiis molestias aperiam, repellat culpa.
+
+      </div>
+
+
+      <div class="container-fluid d-flex flex-row h-50">
+
+        <div class="container w-45 bg-light mh-80">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam laboriosam, neque non repellendus optio
+          pariatur tempore unde ab consequatur qui sapiente, asperiores omnis molestias sunt enim rem quam odit magni.
         </div>
-        <div class="col">
-          One of three columns
+        <div class="container w-45 bg-light mh-80">
+          <img src="../assets/img1.png" class="img-fluid" alt="carcare">
         </div>
+
       </div>
     </div>
 
-    <button type="button" to="/Form" class="btn btn-danger"><router-link to="/Form">Formulario</router-link></button>
-    
+    <!-- Home2 -->
+    <div class="container-fluid text-center vh-90 w-100">
+      <!-- Titulo -->
+      <div class="container text-center mw-80 h-10">
+        <h2 class="mt-4">
+          ¿POR QUÉ ELEGIRNOS?
+        </h2>
+      </div>
+
+      <div class="container d-flex flex-row h-45 justify-content-between">
+
+        <div class="container w-45 bg-light h-95">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam laboriosam, neque non repellendus optio
+          pariatur tempore unde ab consequatur qui sapiente, asperiores omnis molestias sunt enim rem quam odit magni.
+        </div>
+
+        <div class="container w-45 bg-light h-95">
+          <img src="../assets/img2.png" class="img-fluid" alt="carcare">
+        </div>
+
+      </div>
+
+      <div class="container d-flex flex-row h-45 justify-content-between">
+
+        <div class="container w-45 bg-light h-95">
+          <img src="../assets/img3.png" class="img-fluid" alt="carcare">
+        </div>
+
+
+        <div class="container w-45 bg-light h-95">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam laboriosam, neque non repellendus optio
+          pariatur tempore unde ab consequatur qui sapiente, asperiores omnis molestias sunt enim rem quam odit magni.
+        </div>
+
+
+      </div>
+    </div>
+
   </div>
 </template>
 
