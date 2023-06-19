3fea53362d4b0834b3762573fa02652275502f3e
emputed <amejoradan1600@alumno.ipn.mx>
2023-06-18 19:07:47 -0600

    Se modifica la Vista Form

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,20 +1,29 @@
 <template>
-    <Form></Form>
+    <div class="container text-center">
+        <div class="row align-items-center">
+            <div class="col">
+                One of three columns
+            </div>
+            <div class="col">
+                <Form></Form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 
 import Form from '@/components/Form.vue';
-    export default{
-        name: 'FormView',
-        components:{
-            Form
-        },
-        data(){
-            return{
-                autos:[]
-            }
-        },
-        
-    }
+export default {
+    name: 'FormView',
+    components: {
+        Form
+    },
+    data() {
+        return {
+
+        }
+    },
+
+}
 </script>
