85a1d76234e65a6f2aa89862519dce471630ce7d
emputed <amejoradan1600@alumno.ipn.mx>
2023-06-19 12:44:52 -0600

    Modificacion componente Coti, conflicto con imagenes

--- src/components/Coti.vue
+++ src/components/Coti.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <p>{{ year }}</p>
     <table class="table">
       <thead>
@@ -12,38 +12,38 @@
       <tbody>
         <tr>
           <th scope="row">Robo Total</th>
-          <td>Mark</td>
-          <td>  </td>    
+          <td> <img src="../assets/tache.png" height="15%" width="15%"> </td>
+          <td> <img src="../assets/Light_green_check.png" height="10%" width="10%"> </td>    
         </tr>
         <tr>
           <th scope="row">Responsabilidad civil por daños a 3ros</th>
-          <td>Jacob</td>
-          <td>Thornton</td>          
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>          
         </tr>
         <tr>
           <th scope="row">Daños materiales</th>
-          <td>Jacob</td>
-          <td colspan="2">Larry the Bird</td>
+          <td><img src="../assets/tache.png" height="15%" width="15%"></td>
+          <td colspan="2"><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
         <tr>
           <th scope="row">Servicio de Asistencia</th>
-          <td>Jacob</td>
-          <td colspan="2">Larry the Bird</td>
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
+          <td colspan="2"><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
         <tr>
           <th scope="row">Defensa legal</th>
-          <td>Jacob</td>
-          <td colspan="2">Larry the Bird</td>
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
+          <td colspan="2"><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
         <tr>
           <th scope="row">Gastos médicos</th>
-          <td>Jacob</td>
-          <td colspan="2">Larry the Bird</td>
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
+          <td colspan="2"><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
         <tr>
           <th scope="row">Costo</th>
-          <td>Jacob</td>
-          <td colspan="2">hola</td>
+          <td> {{ this.costoEstadar }} </td>
+          <td colspan="2"> </td>
         </tr>
       </tbody>
     </table>
@@ -54,11 +54,26 @@
 import { mapGetters } from "vuex";
 export default {
   name: "Coti",
+  data(){
+    return{
+      costoEstadar: 1,
+      costoPremium: 1,
+      precioDef: 15000,
+      multiplo : 1
+    };
+  },
   computed: {
     ...mapGetters({
       year: "getYear",
     }),
   },
-  methods: {},
+
+  mounted(){
+    if(this.year === "2023" || "2022" || "2021"){
+      this.multiplo = this.precioDef * 0.5
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .2)
+    }
+  },
 };
 </script>
