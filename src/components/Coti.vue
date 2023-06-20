c113f49c6f66427a13b6d14186b71f4d6a356e9b
emputed <amejoradan1600@alumno.ipn.mx>
2023-06-19 23:31:39 -0600

    Problema con el form

--- src/components/Coti.vue
+++ src/components/Coti.vue
@@ -13,12 +13,12 @@
         <tr>
           <th scope="row">Robo Total</th>
           <td> <img src="../assets/tache.png" height="15%" width="15%"> </td>
-          <td> <img src="../assets/Light_green_check.png" height="10%" width="10%"> </td>    
+          <td> <img src="../assets/Light_green_check.png" height="10%" width="10%"> </td>
         </tr>
         <tr>
           <th scope="row">Responsabilidad civil por daños a 3ros</th>
           <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
-          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>          
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
         <tr>
           <th scope="row">Daños materiales</th>
@@ -33,19 +33,22 @@
         <tr>
           <th scope="row">Defensa legal</th>
           <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
-          <td colspan="2"><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
         <tr>
           <th scope="row">Gastos médicos</th>
           <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
-          <td colspan="2"><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
-        </tr>
-        <tr>
-          <th scope="row">Costo</th>
-          <td> {{ this.costoEstadar }} </td>
-          <td colspan="2"> </td>
+          <td><img src="../assets/Light_green_check.png" height="10%" width="10%"></td>
         </tr>
       </tbody>
+    </table>
+
+    <table class="table">
+      <tr>
+        <th scope="row">Costo</th>
+        <td> {{ this.costoEstadar }} </td>
+        <td>{{ this.costoPremium }} </td>
+      </tr>
     </table>
   </div>
 </template>
@@ -54,12 +57,12 @@
 import { mapGetters } from "vuex";
 export default {
   name: "Coti",
-  data(){
-    return{
+  data() {
+    return {
       costoEstadar: 1,
       costoPremium: 1,
-      precioDef: 15000,
-      multiplo : 1
+      precioDef: 18000,
+      multiplo: 1
     };
   },
   computed: {
@@ -68,12 +71,48 @@ export default {
     }),
   },
 
-  mounted(){
-    if(this.year === "2023" || "2022" || "2021"){
+  mounted() {
+    if (this.year === "2023" || "2022" || "2021" ) {
+      this.multiplo = this.precioDef * 0.7
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .3)
+    }
+    else  if (this.year === "2020" || "2019" || "2018" || "2017") {
+      this.multiplo = this.precioDef * 0.6
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .3)
+    }
+    /*else  if (this.year === "2016" || "2015" || "2014" || "2013" || "2012") {
       this.multiplo = this.precioDef * 0.5
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .3)
+    }
+    else  if (this.year === "2011" || "2010" || "2009" || "2008" || "2007") {
+      this.multiplo = this.precioDef * 0.4
       this.costoPremium = this.precioDef + this.multiplo
       this.costoEstadar = this.precioDef + (this.precioDef * .2)
     }
+    else  if (this.year === "2006" || "2005" || "2004" || "2003" || "2002" || "2001") {
+      this.multiplo = this.precioDef * 0.3
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .2)
+    }
+    else  if (this.year === "2000" || "1999" || "1998" || "1997" || "1996" || "1995" || "1994") {
+      this.multiplo = this.precioDef * 0.2
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .2)
+    }
+    else  if (this.year === "1993" || "1992" || "1991" || "1990" || "1989" || "1988" || "1987" || "1986" || "1985") {
+      this.multiplo = this.precioDef * 0.2
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .1)
+    }
+    else  if (this.year === "1984" || "1983" || "1982" || "1981" || "1980" || "1979" || "1978" || "1977" || "1976" || "1975") {
+      this.multiplo = this.precioDef * 0.2
+      this.costoPremium = this.precioDef + this.multiplo
+      this.costoEstadar = this.precioDef + (this.precioDef * .1)
+    }*/
+
   },
 };
 </script>
--- src/views/CotiView.vue
+++ src/views/CotiView.vue
@@ -1,13 +1,16 @@
 <template>
+    <NavBar></NavBar>
     <Coti/>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
 import Coti from '@/components/Coti.vue';
 export default{
     name: 'CotiView',
     components:{
-        Coti
+        Coti,
+        NavBar
     }
 }
 </script>
